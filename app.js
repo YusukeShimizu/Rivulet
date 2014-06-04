567954488aee8f6f53e3b355374d8396f010f0a8
bluebird <stm1051212@yahoo.co.jp>
2014-06-04 20:44:56 +0900

    arrange code

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -11,12 +10,13 @@ var path = require('path');
 
 var app = express();
 
-//è¿½åŠ ãƒ¢ã‚¸ãƒ¥ãƒ¼ãƒ«ã®ãƒ­ãƒ¼ãƒ‰
-var config = require('./add_modules/config.js').config;
+//’Ç‰Áƒ‚ƒWƒ…[ƒ‹‚Ìƒ[ƒh
+var config = require('./lib/config.js').config;
 var setting = require('./lib/setting');
+var viewController = require('./lib/viewController');
 
-setting.passportSerialize();
-setting.passportUse(config);
+//node_module‚ÌƒZƒbƒgƒAƒbƒv
+setting.init(app,config);
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -30,7 +30,7 @@ app.use(express.methodOverride());
 app.use(express.cookieParser('your secret here'));
 app.use(express.session());
 app.use(express.cookieParser());
-app.use(express.session({secret: "Candy"}));
+app.use(express.session({secret: config.MEMORY_STORE}));
 
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
@@ -43,12 +43,12 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
+//ƒRƒ“ƒgƒ[ƒ‰‚ÌƒZƒbƒgƒAƒbƒv
+viewController.init(app,setting.passport);
+
 server = http.createServer(app);
 
 server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
-
-
-
 
