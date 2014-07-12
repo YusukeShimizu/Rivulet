22ba6a5ef7b9b22482baebb5729662b18302a8a5
bluebird <stm1051212@yahoo.co.jp>
2014-07-12 09:49:39 +0900

    reconnect functions

--- public/lib/stream/client.js
+++ public/lib/stream/client.js
@@ -1,22 +1,93 @@
-
 //use socket.io(0.9.16)to enable real time communication
 (function(){
     var client = {};
-    // socket.io is loaded in page
-    var socket = io.connect('http://localhost');
+    var connectFail  = 0; 
+    var connectCount = 0;
+    var streamFailCount = 0;
+    var pingTimeout;
+    var interval;
 
-    //candy always use JSON
-    function send(data){
-        socket.emit('message', JSON.stringify(data));
+    client.connect = function(cb){
+        var wait = (connectFail < 0 ? 0 : connectFail) * 1000;
+        var max  = 30 * 1000;
+        if(wait > max) {
+            wait = max;
+        }
+        console.log("[Connect] waiting for "+wait);
+        setTimeout(function () {
+            _connect(cb);
+        }, wait);
     }
 
-    client.connect = function(connect){
+    function _connect(cb){
+        // socket.io is loaded in page
+        var socket = io.connect('http://localhost');
         socket.on('message', connect);
+
+        var failed = false;
+        var connected = true;
+
+        //always use JSON
+        function send(data){
+            socket.emit('message', JSON.stringify(data));
+        }
+        //make this globally accessible
+        client.send = function(data){
+            send(data);
+        }
+
+        socket.on('message', function (msg) {
+            if(failed) {
+                console.log("[Backend] Ignoring messages after fail "+msg)
+                return;
+            }
+            var data = JSON.parse(msg);
+            if(data == "pong") {
+                // we have a successful connection
+                connectFail = -1; 
+                connected = true;
+            }
+            else if(data.message) {
+                failed = true;
+                console.log("Backend error:"+ data.message);
+                // We have an error on the backend connection to twitter.
+                // Wait a short time and then reconnect.
+                setTimeout(function () {
+                    if(pingTimeout) {
+                        clearTimeout(pingTimeout);
+                    }
+                    clearInterval(interval);
+                    socket.disconnect();
+                    console.log("[Connect] Reconnecting after error "+streamFailCount);
+                    connect(cb);
+                }, 1000 * (++streamFailCount))
+            }
+        });
+
+        function ping() { // send a ping every N seconds
+            connected = false;
+            send('ping');
+            if(pingTimeout) {
+                clearTimeout(pingTimeout);
+            }
+
+            // if within 3 seconds there was no pong we assume the socket is gone and reconnect
+            pingTimeout = setTimeout(function () { 
+                if(!connected) {
+                    clearInterval(interval);
+                    connectFail++;
+                    socket.disconnect(); 
+                    console.log("[Connect] Reconnecting after connection failure");
+                    connect(cb);
+                }
+            }, 3000)
+        }
+        if(interval) {
+            clearInterval(interval);
+        }
+        interval = setInterval(ping, 5000);
+        ping();
     }
 
-    client.send = function(data){
-        send(data);
-    }
     window.client = client;
 })();
-
--- public/lib/stream/initplugins.js
+++ public/lib/stream/initplugins.js
@@ -14,9 +14,49 @@
     settings.registerKey("notifications", "sound", "Play a sound for new tweets",  false);
 
     var plugins = {
+        //when location.hash change set the class of html body
+        hashState: {
+            ScrollState :{},
+            StyleAppend :{},
+            func : function hashState(stream,plugin){
+                var win = $(window);
+                function change(){
+                    var val = location.hash.replace(/^\#/, "");
+                    $("body").attr("class",val);
+                    var scrollState = plugin.ScrollState[val || "all"];
+                    if(scrollState) != null){
+                        win.scrollTop(scrollState);
+                    }
+                    if(!plugin.StyleAppended[val] && val != "all") {
+                        plugin.StyleAppended[val] = true;
+                        var className = val.replace(/[^\w-]/g, "");
+                        // to hide everything besides things tagged with the current state
+                        var style = '<style type="text/css" id>'+
+                            'body.'+className+' #content #stream li {display:none;}\n'+
+                            'body.'+className+' #content #stream li.'+className+' {display:block;}\n'+
+                            '</style>';
+                        style = $(style);
+                        $("head").append(style);
+                    }
+                }   
+                win.bind("hashchange", change);
+                change();
+                var scrollTimer;
+                function scrollTimeout() {
+                    window.Streamie_Just_Scrolled = false;
+                }
+                win.bind("scroll", function () {
+                    plugin.ScrollState[location.hash.replace(/^\#/, "") || "all"] = win.scrollTop();
+                    window.Streamie_Just_Scrolled = true;
+                    clearTimeout(scrollTimer);
+                    scrollTimer = setTimeout(scrollTimeout, 1000);
+                });
+            }
+        },
+
         // click item activate
         navigation: {
-            func: function navigation(plugin){
+            func: function navigation(stream,plugin){
                 // mainstatus 
                 $("#mainstatus").on("close",function(){
                     if($("#mainstatus").hasclass("show")){
@@ -36,13 +76,14 @@
             }
         },
         personalizeForCurrentUser: {
-            func: function personalizeForCurrentUser (stream) {
+            func: function personalizeForCurrentUser (stream,plugin) {
                 //display screenname
                 $("#currentuser-screen_name").text("@"+stream.user.screen_name);
             }
         }
     }
     window.initPlugins = [
+        plugins.hashState,
         plugins.navigation,
         plugins.personalizeForCurrentUser
     ];
