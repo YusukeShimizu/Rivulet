de47d3f07f190eacf4b77eaf5ccd882a7ff25029
bluebird <stm1051212@yahoo.co.jp>
2014-09-13 14:29:01 +0900

    merge stream.js to app.js

--- app.js
+++ app.js
@@ -12,15 +12,19 @@ var path = require('path');
 var passport = require('passport');
 var TwitterStrategy = require('passport-twitter').Strategy;
 var config = require(__dirname + '/lib/config.js').config;
+var templates = require(__dirname + '/lib/templates.js');
+var twitter_connector = require(__dirname + '/lib/twitter_connector.js');
 
-var app = module.exports = express();
+var app = express();
 var server = http.createServer(app);
 
 // using socket.io
-var io = require('socket.io').listen(server);    
-var stream = require(__dirname + '/lib/stream.js');
-var sessionStore = express.session.MemoryStore();
+var io = require('socket.io').listen(server);
 
+var sessionStore = new express.session.MemoryStore();
+var cookieParser = require('cookie-parser');
+var parseCookie = cookieParser('cookieSessionKey');
+    
 //Passport will serialize and deserialize user instances to and from the session
 passport.serializeUser(function(user, done) {
     done(null, user);
@@ -55,9 +59,9 @@ app.use(express.urlencoded());
 app.use(express.methodOverride());
 
 //validate session middleware
-app.use(express.cookieParser("cookieParser"));
+app.use(express.cookieParser(app.get("cookieSessionKey")));
 app.use(express.session({
-    key : app.get('cookieSessionKey'),     
+    secret : 'cookieSessionKey',     
     store : sessionStore
 }));
 app.use(passport.initialize());
@@ -87,7 +91,94 @@ server.listen(app.get('port'), function(){
     console.log('Express server listening on port ' + app.get('port'));
 });
 
+var cookieParser = require('cookie-parser');
+var parseCookie = cookieParser('cookieSessionKey');
+    
 // share session between socket.io and express
-io.set('authorization', stream.init);
+io.configure(function(){
+	io.set('authorization', function(handshakeData, callback) {
+
+    	if (handshakeData.headers.cookie) {
+
+            parseCookie(handshakeData, null, function(err) {
+                if (err) {
+                    return accept('Error parseCookie.', false);
+                }
+                var sessionID = handshakeData.signedCookies["cookieSessionKey"];
+ 
+                sessionStore.get(sessionID, function(err, session) {
+                    if (err) {
+                        console.dir(err);
+                        callback(err.message, false);
+                    }
+                    else if (!session) {
+                        console.log('session not found');
+                        callback('session not found', false);
+                    }
+                    else {
+                        console.log("authorization success");
+     
+                        // set session things to handShakeData
+                        handshakeData.cookie = cookie;
+                        handshakeData.sessionID = sessionID;
+                        handshakeData.sessionStore = sessionStore;
+                        handshakeData.session = new Session(handshakeData, session);
+     
+                        callback(null, true);
+                    }
+                });
+            });
+        }
+    	else{
+        	return callback('cookie not found', false);
+    	}
+	});
+});
+
 // connect clients 
-io.sockets.on('connection',stream.connect);
+io.sockets.on('connection',function(socket){
+
+    //always use JSON
+    function send(data){
+        socket.emit('message', JSON.stringify(data));
+    }
+
+    socket.on('message',function(data){     
+        
+        data = JSON.parse(data);
+        console.log(data);    
+
+        if(data == 'ping'){
+            send('pong');
+        }
+        else if(socket.handshake.session.user){
+            send({
+                action: "auth_OK",
+                templates: templates,
+                info: socket.handshake.session.user._json
+            });
+
+            var subscription = twitter_connector.subscribe(socket.handshake.session.user._json.user_id,function(data){
+                send({
+                    tweet: data
+                });
+            },{
+                // set requester
+                consumer_key: config.TWITTER_CONSUMER_KEY,
+                consumer_secret: config.TWITTER_CONSUMER_SECRET,
+                access_token_key: socket.handshake.session.user.twitter_token,
+                access_token_secret: socket.handshake.session.user.twitter_token_secret
+            });
+        }
+    });
+
+    console.log('session data', socket.handshake.session);
+    var sessionReloadIntervalID = setInterval(function() {
+        socket.handshake.session.reload(function() {
+            socket.handshake.session.touch().save();
+        });
+    }, 60 * 2 * 1000);
+    socket.on("disconnect", function(message) {
+        clearInterval(sessionReloadIntervalID);
+    });
+});
---
+++ public/lib/stream/tracking.js
@@ -0,0 +1,49 @@
+/*
+ * Does anonymous tracking of usage
+ */
+
+{function(settings) {
+    
+    settings.registerNamespace("tracking", "Tracking");
+    settings.registerKey("tracking", "allowed", "Allow anonymous tracking of my usage of Streamie",  true);
+    
+    var events = [
+        "status:send", 
+        "tweet:new",
+        "state:conversation",
+        "state:mention",
+        "state:retweet",
+        "state:starred",
+        "state:direct",
+        "status:retweet",
+        "status:delete",
+        "status:favorite",
+        "status:favoriteDestroy"
+    ];
+    
+    function track(event, value) {
+        if(settings.get("tracking", "allowed")) {
+
+            // split up the parts of the custom event
+            var parts = event.split(":"); 
+            var key   = parts[1];
+            key = key.replace(/\d+/g, "");
+        
+            if(typeof _gaq != "undefined") {
+                _gaq.push(['_trackEvent', parts[0], key, event, value]);
+            }
+        }
+    }
+    
+    events.forEach(function (name) {
+        $(document).bind(name, function (e, value) {
+            track(name, value);
+        });
+    });
+    $(document).bind("streamie:activeuse", function (e, screen_name) {
+        track("activeuse:" + screen_name);
+    });
+       
+    return {}
+    }
+})();
