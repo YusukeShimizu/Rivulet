de98e52d3d9c1115b955eefb776669946fc176d9
bluebird <stm1051212@yahoo.co.jp>
2014-09-19 20:56:15 +0900

    routes rest api

--- app.js
+++ app.js
@@ -12,18 +12,18 @@ var path = require('path');
 var passport = require('passport');
 var TwitterStrategy = require('passport-twitter').Strategy;
 var config = require(__dirname + '/lib/config.js').config;
-var templates = require(__dirname + '/lib/templates.js');
-var twitter_connector = require(__dirname + '/lib/twitter_connector.js');
 
 var app = express();
 var server = http.createServer(app);
 
 // using socket.io
 var io = require('socket.io').listen(server);
-
 var sessionStore = new express.session.MemoryStore();
 var cookieParser = express.cookieParser("cookieParser");
-    
+var connection = require(__dirname + '/lib/connection.js');
+
+connection.init(config,sessionStore,cookieParser);
+
 //Passport will serialize and deserialize user instances to and from the session
 passport.serializeUser(function(user, done) {
     done(null, user);
@@ -65,6 +65,7 @@ app.use(express.session({
 }));
 app.use(passport.initialize());
 app.use(passport.session()); 
+app.use(express.bodyParser());
 
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
@@ -74,11 +75,14 @@ if ('development' == app.get('env')) {
     app.use(express.errorHandler());
 }
 
-// main app view
+// main app routes
 app.get('/', routes.index);
 app.get('/users', user.list);
-// passport Routes  
-app.get("/header", routes.header);
+app.get('/header', routes.header);
+app.post('/tweet',routes.tweet);
+app.post('/favorite',routes.favorite);
+app.post('/delete',routes.delete);
+app.post('/retweet',routes.retweet);
 app.get('/logout/twitter', routes.logout);
 app.get("/auth/twitter", passport.authenticate('twitter'));
 app.get("/auth/twitter/callback", passport.authenticate('twitter', {
@@ -86,106 +90,15 @@ app.get("/auth/twitter/callback", passport.authenticate('twitter', {
     failureRedirect: '/'
 }));
 
+
 server.listen(app.get('port'), function(){
     console.log('Express server listening on port ' + app.get('port'));
 });
 
 // share session between socket.io and express
 io.configure(function(){
-	io.set('authorization', function(handshakeData, callback) {
-
-    	if (handshakeData.headers.cookie) {
-
-            cookieParser(handshakeData, null, function(err) {
-                if (err) {
-                    return accept('Error parseCookie.', false);
-                }
-                var sessionID = handshakeData.signedCookies["connect.sid"];
- 
-                sessionStore.get(sessionID, function(err, Session) {
-                    if (err) {
-                        console.dir(err);
-                        callback(err.message, false);
-                    }
-                    else if (!Session) {
-                        console.log('session not found');
-                        callback('session not found', false);
-                    }
-                    else {
-                        console.log("authorization success");
-                        // set session things to handShakeData
-                        handshakeData.session = Session;
-                        
-                        callback(null, true);
-                    }
-                });
-            });
-        }
-    	else{
-        	return callback('cookie not found', false);
-    	}
-	});
+	io.set('authorization', connection.handShake);
 });
 
 // connect clients 
-io.sockets.on('connection',function(socket){
-
-    //always use JSON
-    function send(data){
-        socket.emit('message', JSON.stringify(data));
-    }
-
-    var subscription;
-
-    socket.on('message',function(data){     
-        
-        data = JSON.parse(data);
-        console.log(data);    
-
-        if(data == 'ping'){
-            send('pong');
-        }
-        else if(data == 'no_auth'){
-            if(socket.handshake.session.user){
-                send({
-                    action: "auth_OK",
-                    templates: templates,
-                    info: socket.handshake.session.user._json
-                });
-
-                subscription = twitter_connector.subscribe(socket.handshake.session.user._json.id,function(data){
-                    send({
-                        tweet: data
-                    });
-                },{
-                    // set requester
-                    consumer_key: config.TWITTER_CONSUMER_KEY,
-                    consumer_secret: config.TWITTER_CONSUMER_SECRET,
-                    access_token_key: socket.handshake.session.user.twitter_token,
-                    access_token_secret: socket.handshake.session.user.twitter_token_secret
-                });
-            }else {
-                send('no_auth');
-            }
-        }   
-    });
-    // delete subscription 
-    socket.on('disconnect', function(){
-        if(subscription) {
-            subscription.unsubscribe();
-        }
-    })
-
-    // just making sure
-    /*
-    console.log('session data', socket.handshake.session);
-    var sessionReloadIntervalID = setInterval(function() {
-        socket.handshake.session.reload(function() {
-            socket.handshake.session.touch().save();
-        });
-    }, 60 * 2 * 1000);
-    socket.on("disconnect", function(message) {
-        clearInterval(sessionReloadIntervalID);
-    });
-    */
-});
+io.sockets.on('connection', connection.connect);
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
-
 /*
  * GET home page.
  */
+
+var node_twitter = require('twitter');
+var config = require(__dirname + '/../lib/config.js').config;
 
 exports.index = function(req, res){
   res.render('index', { title: 'Candy' });
@@ -20,3 +22,54 @@ exports.header = function(req,res){
     res.redirect('/');
 };
 
+exports.tweet = function(req,res){
+    var twitter = new node_twitter({
+        consumer_key: config.TWITTER_CONSUMER_KEY,
+        consumer_secret: config.TWITTER_CONSUMER_SECRET,
+        access_token_key: req.user.twitter_token,
+        access_token_secret: req.user.twitter_token_secret
+    })
+    .updateStatus(req.body.tweet, function(data){
+        console.log(data);
+    });
+    res.redirect('/');
+}
+
+exports.favorite = function(req,res){
+    var twitter = new node_twitter({
+        consumer_key: config.TWITTER_CONSUMER_KEY,
+        consumer_secret: config.TWITTER_CONSUMER_SECRET,
+        access_token_key: req.user.twitter_token,
+        access_token_secret: req.user.twitter_token_secret
+    })
+    .createFavorite(req.body.tweet_id,true, function(data){
+        console.log(data);  
+    });
+    res.redirect('/');
+}
+
+exports.delete = function(req,res){
+    var twitter = new node_twitter({
+        consumer_key: config.TWITTER_CONSUMER_KEY,
+        consumer_secret: config.TWITTER_CONSUMER_SECRET,
+        access_token_key: req.user.twitter_token,
+        access_token_secret: req.user.twitter_token_secret
+    })
+    .destroyStatus(req.body.tweet_id, function(data){
+        console.log(data);  
+    });
+    res.redirect('/');
+}
+
+exports.retweet = function(req,res){
+    var twitter = new node_twitter({
+        consumer_key: config.TWITTER_CONSUMER_KEY,
+        consumer_secret: config.TWITTER_CONSUMER_SECRET,
+        access_token_key: req.user.twitter_token,
+        access_token_secret: req.user.twitter_token_secret
+    })
+    .retweetStatus(req.body.tweet_id, function(data){
+        console.log(data);  
+    });
+    res.redirect('/');
+}
