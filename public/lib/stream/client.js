b8c44a30874b185d2be400e4aedd0ffee3f9e620
bluebird <stm1051212@yahoo.co.jp>
2014-09-21 00:33:44 +0900

    local things

--- public/lib/stream/client.js
+++ public/lib/stream/client.js
@@ -8,7 +8,7 @@
     var interval;
     var pingTimeout;
     // socket.io is loaded in page
-    var socket = io.connect('http://candy-twitter.com/');
+    var socket = io.connect('http://localhost');
 
     //candy always use JSON
     function send(data){
